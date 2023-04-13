85dc6e0c24431464e34042e4aa5a4949ca99bc25
wogml6510 <wogml6510@naver.com>
2023-04-13 15:51:02 +0900

    호출시 반환되는 count 조절

--- src/main/java/com/jjh/exam/demo/controller/UserHomeController.java
+++ src/main/java/com/jjh/exam/demo/controller/UserHomeController.java
@@ -5,7 +5,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class UsrHomeController {
+public class UserHomeController {
+	
+	private int count;
+	public UserHomeController() {
+		count = 0;
+	}
+	@RequestMapping("/user/home/getCount")
+	@ResponseBody
+	public int getCount() {
+		return count;
+	}
+	
+	@RequestMapping("/user/home/doSetCount")
+	@ResponseBody
+	public String doSetCount(int count) {
+		this.count = count;
+		return "count의 값이" + this.count + "으로 초기화 되었습니다.";
+	}
+	
 	@RequestMapping("/user/home/main")
 	@ResponseBody
 	public String showMain() {
@@ -24,16 +42,7 @@ public class UsrHomeController {
 		return "잘가~";
 	}
 	
-	private int count;
-	public void UserHomeController() {
-		
-	}
-	@RequestMapping("/user/home/main4")
-	@ResponseBody
-	public int showMain4() {
-		return count++;
-		
-	}
+
 }
 
 
