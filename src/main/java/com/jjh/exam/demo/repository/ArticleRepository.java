a2a2800f7fbe640376aa195335dc46fef7eef8a6
wogml6510 <wogml6510@naver.com>
2023-04-14 18:48:08 +0900

    SQL를 xml파일로 옮김

--- src/main/java/com/jjh/exam/demo/repository/ArticleRepository.java
+++ src/main/java/com/jjh/exam/demo/repository/ArticleRepository.java
@@ -14,22 +14,16 @@ import com.jjh.exam.demo.vo.Article;
 @Mapper
 public interface ArticleRepository {
 	
-	@Select("SELECT * FROM article WHERE id = #{id}")
 	public Article getArticle(@Param("id") int id);	
 
-	@Select("SELECT * FROM article ORDER BY id DESC")
 	public List<Article> getArticles();
 
-	@Insert("INSERT INTO article SET regDate=NOW(), updateDate=NOW(), title=#{title}, `body`=#{body}")
 	public void writeArticle(@Param("title") String title, @Param("body") String body);
 
-	@Delete("DELETE FROM article WHERE id = #{id}")
 	public void deleteArticle(@Param("id") int id) ;
 
-	@Update("UPDATE article SET title = #{title}, `body` = #{body}, updateDate = NOW() WHERE id = #{id}")
 	public void modifyArticle(@Param("id")int id, @Param("title")String title, @Param("body")String body);
 
-	@Select("SELECT LAST_INSERT_ID()")
 	public int getLastInsertId();
 
 }
