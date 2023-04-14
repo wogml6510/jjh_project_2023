29c59eb993728e447f137c99f08d6ca5abc74570
wogml6510 <wogml6510@naver.com>
2023-04-14 17:17:13 +0900

    마이바티스, MySQL JDBC 드라이버 추가, 설정 파일에 DB접속정보 기입

--- src/main/java/com/jjh/exam/demo/repository/ArticleRepository.java
+++ src/main/java/com/jjh/exam/demo/repository/ArticleRepository.java
@@ -1,67 +1,23 @@
 package com.jjh.exam.demo.repository;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.stereotype.Component;
+import org.apache.ibatis.annotations.Mapper;
 
 import com.jjh.exam.demo.vo.Article;
 
-@Component
-public class ArticleRepository {
-	private int articlesLastId;
-	private List<Article> articles;
+@Mapper
+public interface ArticleRepository {
 
-	public ArticleRepository() {
-		articlesLastId = 0;
-		articles = new ArrayList<>();
-	}
 
-	public void makeTestData() {
-		for ( int i = 1; i <= 10; i++ ) {
-			String title = "제목" + i;
-			String body = "내용" + i;
+	public Article getArticle(int id);
 
-			writeArticle(title, body);
-		}
-	}
+	public List<Article> getArticles();
 
-	public Article getArticle(int id) {
-		for ( Article article : articles ) {
-			if ( article.getId() == id ) {
-				return article;
-			}
-		}
+	public Article writeArticle(String title, String body);
 
-		return null;
-	}
+	public void deleteArticle(int id) ;
 
-	public List<Article> getArticles() {
-		return articles;
-	}
-
-	public Article writeArticle(String title, String body) {
-		int id = articlesLastId + 1;
-		Article article = new Article(id, title, body);
-
-		articles.add(article);
-		articlesLastId = id;
-
-		return article;
-	}
-
-	public void deleteArticle(int id) {
-		Article article = getArticle(id);
-
-		articles.remove(article);
-	}
-
-	public void modifyArticle(int id, String title, String body) {
-		Article article = getArticle(id);
-
-		article.setTitle(title);
-		article.setBody(body);
-	}
-
+	public void modifyArticle(int id, String title, String body);
 
 }
