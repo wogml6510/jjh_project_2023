ac71949369f5ecc9682e583100fecd62088ec56e
wogml6510 <wogml6510@naver.com>
2023-04-18 14:27:24 +0900

    범용 보고서 클래스, ResultData 구현

---
+++ src/main/java/com/jjh/exam/demo/vo/ResultData.java
@@ -0,0 +1,36 @@
+package com.jjh.exam.demo.vo;
+
+import lombok.Getter;
+
+public class ResultData {
+	// S-1, S-2.. : 성공
+	// F-1 : 실패
+	@Getter
+	private String resultCode;
+	@Getter
+	private String msg;			// ?
+	@Getter
+	private Object data1;		// 비고란?
+	
+	private ResultData() {
+		
+	}
+	// 
+	public static ResultData from(String resultCode, String msg, Object data1) {
+		ResultData rd = new ResultData();
+		rd.resultCode = resultCode;
+		rd.msg = msg;
+		rd.data1 = data1;
+		
+		return rd;
+	}
+	
+	// 코드가 성공했는지 여부
+	public boolean isSuccess() {
+		return resultCode.startsWith("S-");
+	}
+	// 실패여부
+	public boolean isFail() {
+		return isSuccess() == false;
+	}
+}
