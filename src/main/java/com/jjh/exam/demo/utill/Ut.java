eac7090492c29633b91ea9611dbe81a66d21f91d
wogml6510 <wogml6510@naver.com>
2023-04-20 16:37:33 +0900

    게시물 삭제 후 replace로 이동

--- src/main/java/com/jjh/exam/demo/utill/Ut.java
+++ src/main/java/com/jjh/exam/demo/utill/Ut.java
@@ -20,4 +20,34 @@ public class Ut {
 	public static String f(String format, Object... args) {
 		return String.format(format, args);
 	}
+
+	public static String jsHistoryBack(String msg) {
+		return """
+				<script>
+				const msg = '$s'.trim();
+				if(msg.length > 0){
+					alert(msg);
+				}
+				history.back();
+				</script>				
+				""";
+	}
+	// 메세지 보여주고 나서 이동
+	public static String jsReplace(String msg, String uri) {
+		if ( msg == null){
+			msg ="";
+		}
+		if ( uri == null){
+			uri ="";
+		}
+		return Ut.f("""
+				<script>
+				const msg = '%s'.trim();
+				if(msg.length > 0){
+					alert(msg);
+				}
+				location.replace('%s');
+				</script>				
+				""", msg, uri);
+	}
 }
